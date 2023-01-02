37aab7aefd89db9b4fd0e357f545d2dcac44ff1b
MelihYuvaci <94677369+MelihYuvaci@users.noreply.github.com>
2023-01-02 12:46:35 +0300

    Add Tableview and Custom Cell

--- Covid19/Covid19/Scene/Countries/CountriesVC.swift
+++ Covid19/Covid19/Scene/Countries/CountriesVC.swift
@@ -11,10 +11,14 @@ class CountriesVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var countries = ["a","b","c","d","e","f","g","h","j","k"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "CountryCell", bundle: nil), forCellReuseIdentifier: "ReusableCell")
     }
     
 
@@ -22,5 +26,18 @@ class CountriesVC: UIViewController {
         dismiss(animated: true)
     }
     
+}
 
+extension CountriesVC : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath) as! CountryCell
+        cell.label.text = countries[indexPath.row]
+        return cell
+    }
+    
+    
 }
